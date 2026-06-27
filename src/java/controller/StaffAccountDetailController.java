5cb0da9d8850847b0205c3ab8dbfc2fc70729289
Luu Thuy Linh <luubong2006@gmail.com>
2026-06-28 01:26:00 +0700

    Update NetBeans project libs and remove unused imports

--- src/java/controller/StaffAccountDetailController.java
+++ src/java/controller/StaffAccountDetailController.java
@@ -3,7 +3,6 @@ package controller;
 import dao.StaffAccountDAO;
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
